9fedbaf0e01c87a859ed2efb185c21d818747cd9
burnett <>
2007-05-16 23:57:57 +0000

    lineends

--- healpix/AlmOp.h
+++ healpix/AlmOp.h
@@ -3,62 +3,62 @@
 
 @author M. Roth 
 
-$Header: /nfs/slac/g/glast/ground/cvs/healpix/healpix/AlmOp.h, v0 2007/05/08 21:31:18 mar0 Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/healpix/healpix/AlmOp.h,v 1.1.1.1 2007/05/15 23:22:21 burnett Exp $
 */
 
 #include "../src/base/alm.h"
 
 namespace healpix {
 
-template<typename T> class AlmOp  {
-/**
-@class AlmOp<T>
-@brief Encapsulates the healpix C++ class Alm, a spherical harmonics data array
-see "alm.h" for more information on methods
+    template<typename T> class AlmOp  {
+        /**
+        @class AlmOp<T>
+        @brief Encapsulates the healpix C++ class Alm, a spherical harmonics data array
+        see "alm.h" for more information on methods
 
-Usage: global methods defined in alm_powspec_tools/alm_map_tools/alm_filter_tools
-       are the only necessary reasons for having this class
-@verbatim
- 
-AlmOp<double> al(lmax,mmax);
-Map<double> map(level);
-map2alm_iter(map.map(),al.Alms(),0);
-   
-@endverbatim
+        Usage: global methods defined in alm_powspec_tools/alm_map_tools/alm_filter_tools
+        are the only necessary reasons for having this class
+        @verbatim
 
-The spherical harmonic coefficients of map will then be stored in 'al'
-   
-*/
+        AlmOp<double> al(lmax,mmax);
+        Map<double> map(level);
+        map2alm_iter(map.map(),al.Alms(),0);
 
-public:
-	/**@brief default constructor
-	*/
-	AlmOp<T>();
+        @endverbatim
 
-	/**@brief constructor
-	   @param lmax  maximum multipole moment
-	   @param mmax  maximum order
-	*/
-	AlmOp<T>(int lmax,int mmax);
+        The spherical harmonic coefficients of map will then be stored in 'al'
 
-	/**@brief returns a reference to the embedded spherical harmonics class (for normal function calls)
-	*/
-	Alm<T>* Alms();
+        */
 
-	/**@brief multiplies the corresponding elements of two equal-dimensioned Spherical Harmonic objects
-	*/
-	AlmOp<T> operator*(AlmOp<T> x);
-	
-	/**@brief adds the corresponding elements of two equal-dimensioned Spherical Harmonic objects
-	*/
-	AlmOp<T> operator+(AlmOp<T> x);
+    public:
+        /**@brief default constructor
+        */
+        AlmOp<T>();
 
-	/**@brief returns the conjugate of a spherical harmonic object
-	*/
-	AlmOp<T> conjugate();
+        /**@brief constructor
+        @param lmax  maximum multipole moment
+        @param mmax  maximum order
+        */
+        AlmOp<T>(int lmax,int mmax);
 
-private:
-	Alm<T> m_alm; //HEALpix library spherical harmonic object
-};
+        /**@brief returns a reference to the embedded spherical harmonics class (for normal function calls)
+        */
+        Alm<T>* Alms();
+
+        /**@brief multiplies the corresponding elements of two equal-dimensioned Spherical Harmonic objects
+        */
+        AlmOp<T> operator*(AlmOp<T> x);
+
+        /**@brief adds the corresponding elements of two equal-dimensioned Spherical Harmonic objects
+        */
+        AlmOp<T> operator+(AlmOp<T> x);
+
+        /**@brief returns the conjugate of a spherical harmonic object
+        */
+        AlmOp<T> conjugate();
+
+    private:
+        Alm<T> m_alm; //HEALpix library spherical harmonic object
+    };
 
 } //namespace
