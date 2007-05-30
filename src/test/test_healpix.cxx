35f4051f3b107850a5a05b6c89b2d9d58752860f
mar0 <>
2007-05-30 19:11:30 +0000

    fixed uninitialized variable error, eof error, path error

--- src/test/test_healpix.cxx
+++ src/test/test_healpix.cxx
@@ -1,5 +1,13 @@
+/** @file test_healpix.cxx
+@brief Test program for various healpix routines
+
+@author M. Roth, T. Burnett
+
+$Header: /nfs/slac/g/glast/ground/cvs/healpix/src/test/test_healpix.cxx,v 1.0 2007/05/16 23:57:57 mar0 Exp $
+*/
+
 #include "healpix/Map.h"
-#include "../base/message_error.h"
+#include "src/base/message_error.h"
 #include "healpix/Healpixel.h"
 #include "TestHealpix.h"
 #include "TestHealpixArray.h"
