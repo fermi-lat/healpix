b3e99384ecee32be8ea51b61eebf9db1e1374380
golpa <>
2008-02-20 17:07:39 +0000

    Fixed bug in the way healpix library was added to linking. Cosmetic changes

--- healpixLib.py
+++ healpixLib.py
@@ -1,6 +1,6 @@
-#$Id$
+#$Id: healpixLib.py,v 1.1 2008/02/16 02:37:35 golpa Exp $
 def generate(env, **kw):
-    env.Tool('addLibrary', library = 'healpix')
+    env.Tool('addLibrary', library = ['healpix'])
     env.Tool('astroLib')
     
 def exists(env):
