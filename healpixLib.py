aa742612926cb916970eb25a8e7592f39b2b35ab
cvs2git <>
2008-02-26 19:36:29 +0000

    This commit was manufactured by cvs2svn to create tag 'healpix-01-02-00'.
    Sprout from master 2008-02-19 23:12:03 UTC burnett 'tagging.'
    Cherrypick from master 2008-02-26 19:36:28 UTC glast 'Small typo':
        SConscript
        healpixLib.py
    Cherrypick from master 2008-02-18 20:59:32 UTC glast 'AutoUpdate':
        ChangeLog

--- healpixLib.py
+++ healpixLib.py
@@ -1,6 +1,7 @@
-#$Id$
+#$Id: healpixLib.py,v 1.2 2008/02/20 17:07:39 golpa Exp $
 def generate(env, **kw):
-    env.Tool('addLibrary', library = 'healpix')
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['healpix'])
     env.Tool('astroLib')
     
 def exists(env):
